b77f574275716bdf296bb7c5423031936cad0b66
iovreche <rafael.rodriguez@iovlabs.org>
2023-06-01 22:07:30 +0200

    refactor: use match for error control

--- getting-started/cli/src/main.rs
+++ getting-started/cli/src/main.rs
@@ -11,14 +11,21 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    let mut content = std::io::BufReader::new(std::fs::File::open(&args.path).expect("Could not read file"));
-    
-    let mut line = String::new();
+    // let mut content = std::io::BufReader::new(std::fs::File::open(&args.path).expect("Could not read file"));
 
-    while content.read_line(&mut line).expect("Could not read contents inside file") > 0 {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    let file = match std::fs::File::open(&args.path) {
+        Ok(file) => { file },
+        Err(error) => { panic!("Could not read file, {}", error) }
+    };
+
+    for line in std::io::BufReader::new(file).lines() {
+        match line {
+            Ok(line_content) => {
+                if line_content.contains(&args.pattern){ println!("{}", line_content); }
+            }
+            Err(err) => {
+                eprintln!("Error reading line: {}", err);
+            }
         }
-        line.clear();
     }
 }
