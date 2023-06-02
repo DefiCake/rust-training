5dbdb80268fe1db9b9503a341f5f19af177db69b
DefiCake <rarore92@gmail.com>
2023-06-02 11:13:51 +0200

    feat: use enumerate() to obtain an index in the iterator

--- getting-started/cli/src/main.rs
+++ getting-started/cli/src/main.rs
@@ -11,14 +11,12 @@ struct Cli {
 fn main() {
   let args = Cli::parse();
 
-  // let mut content = std::io::BufReader::new(std::fs::File::open(&args.path).expect("Could not read file"));
-
   let file = match std::fs::File::open(&args.path) {
     Ok(file) => { file }
     Err(error) => { panic!("Could not read file, {}", error) }
   };
 
-  for line in std::io::BufReader::new(file).lines() {
+  for (i, line) in std::io::BufReader::new(file).lines().enumerate() {
     match line {
       Ok(line_content) => {
         if line_content.contains(&args.pattern) {
@@ -26,7 +24,7 @@ fn main() {
         }
       }
       Err(err) => {
-        eprintln!("Error reading line: {}", err);
+        eprintln!("Error reading line: {}: {}", i, err);
       }
     }
   }
