ad303042885d640adfcc8d03d9fc10ec905ff5e3
DefiCake <rarore92@gmail.com>
2023-06-02 11:02:45 +0200

    chore: use prettier-rust formatting

--- getting-started/cli/src/main.rs
+++ getting-started/cli/src/main.rs
@@ -4,28 +4,30 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    pattern: String,
-    path: std::path::PathBuf
+  pattern: String,
+  path: std::path::PathBuf,
 }
 
 fn main() {
-    let args = Cli::parse();
+  let args = Cli::parse();
 
-    // let mut content = std::io::BufReader::new(std::fs::File::open(&args.path).expect("Could not read file"));
+  // let mut content = std::io::BufReader::new(std::fs::File::open(&args.path).expect("Could not read file"));
 
-    let file = match std::fs::File::open(&args.path) {
-        Ok(file) => { file },
-        Err(error) => { panic!("Could not read file, {}", error) }
-    };
+  let file = match std::fs::File::open(&args.path) {
+    Ok(file) => { file }
+    Err(error) => { panic!("Could not read file, {}", error) }
+  };
 
-    for line in std::io::BufReader::new(file).lines() {
-        match line {
-            Ok(line_content) => {
-                if line_content.contains(&args.pattern){ println!("{}", line_content); }
-            }
-            Err(err) => {
-                eprintln!("Error reading line: {}", err);
-            }
+  for line in std::io::BufReader::new(file).lines() {
+    match line {
+      Ok(line_content) => {
+        if line_content.contains(&args.pattern) {
+          println!("{}", line_content);
         }
+      }
+      Err(err) => {
+        eprintln!("Error reading line: {}", err);
+      }
     }
+  }
 }
