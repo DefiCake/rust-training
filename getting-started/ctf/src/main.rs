044316c4480ff68d970ff743128fc110db2ed074
DefiCake <rarore92@gmail.com>
2023-07-13 18:13:35 +0200

    feat: try to generate a specific hash, giving up

--- getting-started/ctf/src/main.rs
+++ getting-started/ctf/src/main.rs
@@ -1,70 +1,54 @@
-use ethers::prelude::rand::thread_rng;
-use sha256::{ digest };
-use ethabi::{ encode, FixedBytes, Token };
+use ethabi::{ encode, ParamType };
 use ethabi::token::{ LenientTokenizer, Tokenizer };
 use ethers::prelude::*;
-
-use ethers::{
-  core::{ types::TransactionRequest, utils::Anvil },
-  middleware::SignerMiddleware,
-  providers::{ Http, Middleware, Provider },
-  signers::{ LocalWallet, Signer },
-};
+use rayon::prelude::*;
 use rand::Rng;
-use ethabi::Token;
 
-#[tokio::main]
-async fn main() {
-  let selector = hex::decode("1626ba7e").unwrap();
-  let hash = hex::decode("265f1ae422b537c21d4f660ba0a1a4d703fc8eba732561832f2070e0a1ecb35e").unwrap();
+use sha2::{ Sha256, Digest };
+
+fn main() {
+  let selector_str = "1626ba7e";
+  let selector = hex::decode(&selector_str).unwrap();
+  // let hash = hex::decode("265f1ae422b537c21d4f660ba0a1a4d703fc8eba732561832f2070e0a1ecb35e").unwrap();
   let private_key = hex::decode("b5c6d3977d2beba1c1cfc61bddd446786db3db7445a8cdf7747115eea578b1f9").unwrap();
-  //   let hash_token = Tokenizer::tokenize_fixed_bytes(hash, 32).unwrap();
-  let hash_token = LenientTokenizer::tokenize_fixed_bytes(
-    "265f1ae422b537c21d4f660ba0a1a4d703fc8eba732561832f2070e0a1ecb35e",
-    32
-  );
+
+  let hash_token = LenientTokenizer::tokenize(
+    &ParamType::FixedBytes(32),
+    "265f1ae422b537c21d4f660ba0a1a4d703fc8eba732561832f2070e0a1ecb35e"
+  ).unwrap();
 
   let wallet = LocalWallet::from_bytes(&private_key).unwrap();
 
   println!("Wallet address is {}", &wallet.address());
 
-  let mut rng = rand::thread_rng();
-  let mut nonce = 0u32;
+  loop {
+    let results: Vec<String> = (1i32..1_000_000)
+      .into_par_iter()
+      .map_init(
+        || rand::thread_rng(), // get the thread-local RNG
+        |rng, _x| {
+          // let mut nonce = rng.gen::<u32>();
+          // let signature = wallet.sign_message(&format!("{}", nonce)).await.unwrap();
+          let signature = hex::encode(rng.gen::<[u8; 32]>());
+          let signature_token = LenientTokenizer::tokenize(&ParamType::Bytes, &signature).unwrap();
+          let encoded = encode(&[hash_token.clone(), signature_token.clone()]);
+          let data = [selector.clone(), encoded].concat();
 
-  nonce = rng.gen::<u32>();
+          let mut hasher: Sha256 = Sha256::new();
+          hasher.update(&data);
 
-  let signature = wallet.sign_message(&format!("{}", nonce)).await.unwrap();
+          let result = hex::encode(hasher.finalize());
 
-  let signature_token: Token = ethabi::Token(LenientTokenizer::tokenize_bytes(&signature.to_string()).unwrap());
+          if result.starts_with(selector_str) {
+            println!("Found result {} with signature", result);
+            println!("Signature {} {}", 32, &signature);
+          }
 
-  let encoded = encode([signature_token]);
+          result
+        }
+      )
+      .collect();
 
-  // loop {
-  //   nonce = rng.gen::<u32>();
-
-  //   let signature = wallet.sign_message(&format!("{}", nonce)).await.unwrap();
-
-  //   let signature_token = LenientTokenizer::tokenize_bytes(&hex::encode(&signature)).unwrap();
-  //   let encoded = encode(hash_token);
-  // }
-
-  // loop {
-  //   let signature = wallet.sign_message(&format!("{}", nonce)).await.unwrap();
-
-  //   let signature_token = Tokenizer::tokenize_bytes(signature).unwrap();
-
-  //   let encoded = encode([&hash_token, &signature_token]);
-  //   // let encoded = arrayify(coder.encode(&[("bytes32", "bytes")], &[(&hash, &signature)])).unwrap();
-
-  //   let mut data: Vec<u8> = [selector, encoded].concat();
-
-  //   let result = digest(&data);
-
-  //   if result[..10] == *selector {
-  //     println!("Found {} {} {} {} {}", result, wallet.private_key().unwrap(), wallet.address(), nonce, signature);
-  //     break;
-  //   }
-
-  //   nonce += 1;
-  // }
+    println!("Ended round with a result {}", results.last().unwrap());
+  }
 }
