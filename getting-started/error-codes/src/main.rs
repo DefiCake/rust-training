bf9bb9946cf0c14eebf51f6fe91a232f1d3c510f
DefiCake <rarore92@gmail.com>
2023-07-29 19:49:53 +0200

    feat: add error codes tutorial

---
+++ getting-started/error-codes/src/main.rs
@@ -0,0 +1,23 @@
+use std::{ fs::File };
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+  path: String,
+}
+
+fn main() {
+  let args: Args = Args::parse();
+  let file = File::open(args.path);
+
+  match file {
+    Ok(_) => {
+      println!("Could open file");
+      std::process::exit(exitcode::OK);
+    }
+    Err(_) => {
+      println!("Could not open path");
+      std::process::exit(exitcode::IOERR);
+    }
+  }
+}
