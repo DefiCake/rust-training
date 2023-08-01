0bc5fcc1dbfca0cdb5fc65e5d39cfa4fa72222e0
DefiCake <rarore92@gmail.com>
2023-08-01 19:16:58 +0200

    feat: wip serializable memory store for database

--- fuel/block-transition-validation/src/serialization/bincode.rs
+++ fuel/block-transition-validation/src/serialization/bincode.rs
@@ -2,9 +2,16 @@ use std::fs::File;
 use std::io::Write;
 use std::io::Read;
 use anyhow::Result;
+use fuel_core::database::Database;
+use fuel_core::database::Column;
+use fuel_core::state::DataSource;
+use fuel_core::state::in_memory::memory_store::MemoryStore;
+use fuel_core::state::Value;
 use fuel_core::types::blockchain::block::Block;
 use fuels::tx::Bytes32;
 use bincode;
+
+use std::{ collections::BTreeMap, fmt::Debug, sync::{ Arc, Mutex } };
 
 use crate::serialization::lib::BinFileSerde;
 
@@ -27,3 +34,33 @@ impl BinFileSerde for Block<Bytes32> {
     Ok(block)
   }
 }
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct MyMemoryStore {
+  inner: [Mutex<BTreeMap<Vec<u8>, Value>>; Column::COUNT],
+}
+
+// #[derive(serde::Serialize, serde::Deserialize)]
+// struct DatabaseWrapper {
+//   database: Database,
+// }
+
+// impl BinFileSerde for DatabaseWrapper {
+//   fn to_bincode_file(&self, path: String) -> Result<()> {
+//     let bin_data = bincode::serialize(&self)?;
+//     let mut file = File::create(&path)?;
+//     file.write_all(&bin_data)?;
+
+//     Ok(())
+//   }
+
+//   fn from_bincode_file(path: String) -> Result<Self> where Self: Sized {
+//     let mut file = File::open(&path)?;
+//     let mut bin_data = Vec::new();
+//     file.read_to_end(&mut bin_data)?;
+
+//     let database: DatabaseWrapper = bincode::deserialize(&bin_data)?;
+
+//     Ok(database)
+//   }
+// }
